70569137204dbcee964ff543fc34282fd4babbbc
mxxt1 <matt@dustcloudlabs.com>
2019-10-31 13:52:23 -0400

    added MSG

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+//first step is to import dotenv and call config
+require('dotenv').config();
 const express = require('express');
 
 const server = express(); // creating an instance of an express server
+
+console.log(process.env.MSG);
 
 server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
