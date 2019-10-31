c52d442962c3a513bc7e518a2081285f83109f60
mxxt1 <matt@dustcloudlabs.com>
2019-10-31 13:22:27 -0400

    updated scripts

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
 });
 
-server.listen(4000, () => {
+const port = process.env.PORT || 4000;
+
+console.log(port);
+
+server.listen(port, () => {
   console.log('\n Server running on port 4000 \n');
 });
